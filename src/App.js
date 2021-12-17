8718b61eefa8544b4a9420fca1a2e226f46ae2bc
Mdidu <alexandre.meddas@gmail.com>
2021-12-17 15:48:37 +0100

    adding categoryList component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,30 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/navigation/Navbar";
+import CategoryList from "./components/UI/CategoryList";
 // import MenuSelect from "./components/UI/MenuSelect";
 
 function App() {
+  const genres = [
+    "Shonen",
+    "Comedy",
+    "Martial Arts",
+    "School Life",
+    "Mystery",
+    "Romance",
+    "Fantasy",
+    "Horror",
+    "Tragery",
+    "Drama",
+  ];
+
   return (
     <div>
       <header>
         <Navbar />
       </header>
-      <Routes>
-        {/* <Route path="/genre/:name" element={<Test />} /> */}
-      </Routes>
+      <CategoryList title="Genres" categories={genres} />
+      <Routes>{/* <Route path="/genre/:name" element={<Test />} /> */}</Routes>
     </div>
   );
 }
