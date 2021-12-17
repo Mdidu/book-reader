c22fd838d8c50889a778267d9631cb5295f96b6f
Mdidu <alexandre.meddas@gmail.com>
2021-12-17 14:46:02 +0100

    adding Navbar

---
+++ src/components/layout/navigation/Navbar.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import BurgerMenu from "../../UI/menu/BurgerMenu";
+import MenuSelect from "../../UI/menu/MenuSelect";
+import styles from "./Navbar.module.css";
+
+const Navbar = () => {
+  const [display, setDisplay] = useState(false);
+
+  const genres = ["Shonen", "War"];
+  const bookListState = [
+    "Latest Release",
+    "Hot Book",
+    "Completed Book",
+    "Most Popular",
+  ];
+
+  const hiddenClass = display ? "" : styles.hidden;
+
+  const displayMenu = () => {
+    setDisplay(!display);
+  };
+
+  return (
+    <nav>
+      <ul>
+        <li>BOOK</li>
+        <div className={`${styles.allMenu} ${hiddenClass}`}>
+          <MenuSelect title="Book List ▼" datas={bookListState} />
+          <MenuSelect title="Genre ▼" datas={genres} route={"genre"} />
+        </div>
+      </ul>
+      <BurgerMenu onClick={displayMenu} />
+    </nav>
+  );
+};
+
+export default Navbar;
